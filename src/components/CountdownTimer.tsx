803b5cd4c2115fe1edf19de9ebde1e7310f95138
mvotho <jackfaulknerbis@hotmail.co.uk>
2022-09-19 22:36:23 +0100

    added styling to countdown timer

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -1,28 +1,44 @@
 import { useEffect, useState } from "react";
 import getDuration from "../util/ultis";
 
-const CountdownTimer = ({race}:any) => {
-  
-	    let d2:any = new Date().getTime();
-	    const diff = (race.trackDate-d2);
+const CountdownTimer = ({ race }: any) => {
 
-      const [timeLeft, setTimeLeft] = useState(getDuration(diff));
+  let d2: any = new Date().getTime();
+  const diff = (race.trackDate - d2);
 
-      useEffect(() => {
-        const timer = setTimeout(() => {
-          setTimeLeft(getDuration(diff));
-        }, 1000);
-        return () => clearTimeout(timer);
-      });
-   
+  const [timeLeft, setTimeLeft] = useState(getDuration(diff));
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(getDuration(diff));
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
+
 
   return (
-    <div>
-      
-      <div>Days: {timeLeft.days} Hours: {timeLeft.hours} Mins: {timeLeft.minutes} Seconds: {timeLeft.seconds}</div>
-      <div>Until -&gt; {race.name}</div>
+    <div className="flex flex-col items-center justify-center pb-6">
+      <h2>Time till the {race.name}</h2>
+      <table className='[&_th]:p-2'>
+        <thead className='' >
+          <tr>
+            <th>{timeLeft.days}</th>
+            <th>{timeLeft.hours}</th>
+            <th>{timeLeft.minutes}</th>
+            <th>{timeLeft.seconds}</th>
+          </tr>
+        </thead>
+        <tr>
+          <th>Days</th>
+          <th>Hours</th>
+          <th>Mins</th>
+          <th>Seconds</th>
+        </tr>
+      </table>
+
+
     </div>
-    
+
   )
 }
 

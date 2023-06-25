c0c1ed7aa83db00c6a59f6c06660e22622a1ac56
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-06-25 16:20:58 +0000

    Updated driver information

--- src/components/DriverConstructors.tsx
+++ src/components/DriverConstructors.tsx
@@ -3,26 +3,38 @@ import React from 'react'
 import { fetchDriverConstructors } from '../api';
 import ConstructorStandings from './ConstructorStandings';
 
-const DriverConstructors = (driver:any) => {
-    
-    console.log(driver.driver);
-    const selectedDriver = driver.driver;
+const DriverConstructors = (driver: any) => {
 
-    const { data, error, isLoading }: any = useQuery(["driverConstructors", selectedDriver], () => fetchDriverConstructors(selectedDriver));
-  
+  console.log(driver.driver);
+  const selectedDriver = driver.driver;
 
-    if (error) return <div>Request Failed</div>;
-    if (isLoading) return <div>Loading...</div>;
-  
+  const { data, error, isLoading }: any = useQuery(["driverConstructors", selectedDriver], () => fetchDriverConstructors(selectedDriver));
 
-    const cons = data.MRData.ConstructorTable.Constructors.map((data:any)=>{
-      return(<div>
-          <li>{data.name}, {data.nationality}</li>
-        </div>
-      )
-    })
+
+  if (error) return <div>Request Failed</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+
+  const cons = data.MRData.ConstructorTable.Constructors.map((data: any) => {
     return (
-    <div>{cons}</div>
+
+      <div className='mt-2'>
+        <div>{data.name}, {data.nationality}</div>
+      </div>
+    )
+  })
+  return (
+    <div>
+      <table className='table-primary  [&_th]:table-primary [&_th]:p-2 text-sm mt-2'>
+        <thead className='table-primary bg-slate-200' >
+          <tr>
+            <th>Constructors</th>
+          </tr>
+        </thead>
+        {cons}
+      </table>
+
+    </div>
   )
 }
 
--- src/components/DriverInfo.tsx
+++ src/components/DriverInfo.tsx
@@ -2,29 +2,44 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 import { fetchDriver } from '../api';
 
-const DriverInfo = (driver:any) => {
-    console.log(driver.driver);
-    const selectedDriver = driver.driver;
-    
-    const { data, error, isLoading }: any = useQuery(["driverInfo", selectedDriver], () => fetchDriver(selectedDriver));
-  
+const DriverInfo = (driver: any) => {
+  console.log(driver.driver);
+  const selectedDriver = driver.driver;
 
-    if (error) return <div>Request Failed</div>;
-    if (isLoading) return <div>Loading...</div>;
-  
-    const driverInfo = data.MRData.DriverTable.Drivers[0];
-    console.log(data.MRData.DriverTable.Drivers[0].driverId);
-    
-    return (
+  const { data, error, isLoading }: any = useQuery(["driverInfo", selectedDriver], () => fetchDriver(selectedDriver));
+
+
+  if (error) return <div>Request Failed</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  const driverInfo = data.MRData.DriverTable.Drivers[0];
+  console.log(data.MRData.DriverTable.Drivers[0].driverId);
+
+  return (
     <div>
       <h1 className='text-lg pb-2'>Driver Information</h1>
-      <li>{driverInfo.driverId} </li>
-      <li>{driverInfo.permanentNumber}</li>
-      <li>{driverInfo.code}</li>
-      <li> {driverInfo.givenName}</li>
-      <li>{driverInfo.familyName}</li>
-      <li>{driverInfo.dateOfBirth}</li>
-      <li>{driverInfo.nationality}</li>
+      <table className='table-primary  [&_th]:table-primary [&_th]:p-2 text-sm'>
+        <thead className='table-primary bg-slate-200' >
+          <tr>
+            <th>Driver ID</th>
+            <th>Driver Number</th>
+            <th>Code</th>
+            <th>Name</th>
+            <th>Family Name</th>
+            <th>DOB</th>
+            <th>Nationality</th>
+          </tr>
+          <tr>
+            <th>{driverInfo.driverId}</th>
+            <th>{driverInfo.permanentNumber == null ? "N/A" : driverInfo.permanentNumber}</th>
+            <th>{driverInfo.code == null ? "N/A" : driverInfo.code}</th>
+            <th>{driverInfo.givenName == null ? "N/A" : driverInfo.givenName}</th>
+            <th>{driverInfo.familyName == null ? "N/A" : driverInfo.familyName}</th>
+            <th>{driverInfo.dateOfBirth == null ? "N/A" : driverInfo.dateOfBirth}</th>
+            <th>{driverInfo.nationality == null ? "N/A" : driverInfo.nationality}</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   )
 }
